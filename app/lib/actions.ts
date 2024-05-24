815b8b0f510d4a7d44d6083c675e5cd582d88373
Michael Cruz <mcruz@aersale.com>
2024-05-24 12:10:12 -0400

    implimenting zod val in createInvoice post

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,13 +1,31 @@
 'use server';
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
 
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+// TODO: error handling
 export async function createInvoice(formData: FormData) {
-  const rawFormData = {
+  const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
-  };
-  const rawFormData2 = Object.fromEntries(formData.entries());
-  // Test it out:
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+  const rawFormData = Object.fromEntries(formData.entries());
   console.log(rawFormData);
-  console.log(rawFormData2);
+
+  await sql`
+  INSERT INTO invoices (customer_id, amount, status, date)
+  VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+`;
 }
